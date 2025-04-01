bebe9aca60e6383ad6c8197fd35c552b3751eb35
Juan Camilo Cuspián <cuspianhuertas@gmail.com>
2025-04-01 08:32:21 -0500

    swsed

---
+++ api/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Desactiva el límite de tiempo de ejecución
+set_time_limit(0);
+
+// Load composer
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
